dffed8ccb83a7fc1641208d75022bcc018cfc5bf
showkatbinidris70 <showkatbinidris70@gmail.com>
2023-11-26 09:28:41 +0600

    modified some code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,3 @@
-// dark mode start
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-
-  var isDarkMode = element.classList.contains("dark-mode");
-  localStorage.setItem("bg_color", isDarkMode);
-}
-
-// Function to set initial dark mode based on localStorage
-function setInitialDarkMode() {
-  var element = document.body;
-  var storedDarkMode = localStorage.getItem("bg_color");
-  if (storedDarkMode === "true") {
-    element.classList.add("dark-mode");
-  } else {
-    element.classList.remove("dark-mode");
-  }
-}
-
-// Set initial dark mode on page load
-document.addEventListener("DOMContentLoaded", function () {
-  setInitialDarkMode();
-});
-// dark mode end
-
-// app info js code start
 function togglePopup() {
   var popup = document.getElementById("popup");
   var overlay = document.getElementById("overlay");
@@ -36,10 +9,4 @@ function togglePopup() {
     overlay.style.display = "none";
   }
 }
-// app info js code end
 
-// data table start
-$(document).ready(function () {
-  $("#example").DataTable();
-});
-// data table end
